d5c2ac04c11b2890ac1b99f6341f7c73a7718651
Fien Strijthaegen <fiens@Karen>
2022-12-24 12:37:26 +0100

    Ran FluTE on Ubuntu I changed the pre-vaccination and mako scripts slightly to get them running. The results of FluTE can be found in work_dir. I don't know yet what all the output files are for. My next task is figuring out how to get all this working together with the scala code that runs the bandits

--- FluTE-bandits/rl_dir/configs/mako-render.py
+++ FluTE-bandits/rl_dir/configs/mako-render.py
@@ -11,8 +11,9 @@ for arg in sys.argv[2:]:
     else:
         print('Error: invalid argument \"%s\"' % arg)
 
+
 mytemplate = Template(filename=template_fn, strict_undefined=True, \
                         default_filters=['decode.utf8'], \
                         input_encoding='utf-8', \
                         output_encoding='utf-8')
-print(mytemplate.render(**args))
+print(mytemplate.render(**args).decode())
--- FluTE-bandits/scripts/bandits/pre-vaccination.sh
+++ FluTE-bandits/scripts/bandits/pre-vaccination.sh
@@ -12,25 +12,30 @@ priorities_no_commas=`echo ${priorities} | sed 's/,//g'`
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#echo "workdir:${workdir}"
-#echo "flute_rl_dir:${flute_rl_dir}"
-#echo "flute_rl_bin:${flute_rl_bin}"
-#echo "config_template:${config_template}"
-#echo "seed:${seed}"
-#echo "r0:${r0}"
-#echo "run_length:${run_length}"
-#echo "geo:${geo}"
-#echo "doses:${doses}"
-#echo "priorities:${priorities}"
+echo "workdir:${workdir}"
+echo "flute_rl_dir:${flute_rl_dir}"
+echo "flute_rl_bin:${flute_rl_bin}"
+echo "config_template:${config_template}"
+echo "seed:${seed}"
+echo "r0:${r0}"
+echo "run_length:${run_length}"
+echo "geo:${geo}"
+echo "doses:${doses}"
+echo "priorities:${priorities}"
 
 cd ${work_dir}
 
+echo "Creating config file..."
 #create config file
-python ${flute_rl_dir}/configs/mako-render.py ${config_template} --label="l0" --seed=${seed} --R0=${r0} --run_length=${run_length} --data_file=${flute_rl_dir}/data/${geo}/${geo} --doses=${doses} --vaccine_priorities=${priorities} > pre.config
+python $(dirname $PWD)/${flute_rl_dir}/configs/mako-render.py $(dirname $PWD)/rl_dir/${config_template} --label="l0" --seed=${seed} --R0=${r0} --run_length=${run_length} --data_file=$(dirname $PWD)/data/${geo}/${geo} --doses=${doses} --vaccine_priorities=${priorities} > pre.config
+echo "Created config file."
 
+echo "Running FluTE..."
 #run flute
-${flute_rl_bin} pre.config > ${work_dir}/out 2> ${work_dir}/err
+$(dirname $PWD)/${flute_rl_bin} pre.config > out 2> err
+echo "Done running FluTE."
 
+echo "Parsing attack rate..."
 #parse attack rate
-ar=`python ${script_dir}/parse_attack_rate.py ${work_dir}/Summary0`
+ar=`python ${script_dir}/parse_attack_rate.py Summary0`
 echo $ar
