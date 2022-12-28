6c9f8c299a75af16c85847e552146321393820c1
ibos0002 <115453894+ibos0002@users.noreply.github.com>
2022-12-28 18:03:02 +0100

    Update epsilon_greedy.py

--- python-bandits/epsilon_greedy.py
+++ python-bandits/epsilon_greedy.py
@@ -3,8 +3,12 @@ from random import randint
 
 class Epsilon_Greedy_Agent():
     """Class that implements the epsilon-greedy bandit, with nbr_actions arms and constant epsilon"""
-    def __init__(self,epsilon:float,nbr_actions:int):
-        self.epsilon = epsilon
+    def __init__(self,nbr_actions:int,epsilon_max:int,
+                 epsilon_decay:float=0,epsilon_min:float=0):
+        self.epsilon = epsilon_max
+        self.epsilon_max = epsilon_max
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
         self.nbr_actions = nbr_actions
         self.q_table = np.zeros(nbr_actions)
         self.repeats = np.zeros(nbr_actions) #number of times each action has been done
