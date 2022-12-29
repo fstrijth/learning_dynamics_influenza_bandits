ae2132aa18a440331f66112e72c11f86e7e44742
ibos0002 <115453894+ibos0002@users.noreply.github.com>
2022-12-29 20:51:29 +0100

    Add softmax and reward plot

--- python-bandits/example_training.py
+++ python-bandits/example_training.py
@@ -1,11 +1,12 @@
 from epsilon_greedy import Epsilon_Greedy_Agent
 from UCB1 import UCB1_Agent
-from plotting import strategies_violin_plots
+from softmax_boltzmann import Softmax_Boltzmann_Agent
+from plotting import strategies_violin_plots, reward_plot
 
 from typing import Union
 from numpy import zeros
 from numpy.random import normal
-from random import randint
+from random import uniform
 
 class NormalGame:
     """
@@ -14,8 +15,8 @@ class NormalGame:
     def __init__(self):
         self.num_agents = 1
         self.num_actions = 10
-        self.avg = [randint(5,25) for _ in range(self.num_actions)]
-        self.std = [randint(1,5) for _ in range(self.num_actions)]
+        self.avg = [uniform(5,10) for _ in range(self.num_actions-1)]+[20]
+        self.std = [uniform(1,2) for _ in range(self.num_actions)]
     def act(self, action: int):
         """
         Method to perform an action in the Matrix Game and obtain the associated reward.
@@ -28,15 +29,17 @@ class NormalGame:
 
 
 def train_agent(env,agent:Union[Epsilon_Greedy_Agent,UCB1_Agent],num_episodes:int):
-    returns = zeros(num_episodes)
+    eval_returns = zeros(num_episodes)
     for t in range(num_episodes):
         action = agent.act(True)
         rew = env.act(action)
-        returns[t] = rew
         agent.learn(rew,action)
+        action = agent.act()
+        rew = env.act(action)
+        eval_returns[t] = rew
         if type(agent) == Epsilon_Greedy_Agent:
             agent.epsilon = max(agent.epsilon_min,agent.epsilon*agent.epsilon_decay)
-    return returns
+    return eval_returns
 
 def outcome_distribution(env,action:int,num_test:int):
     dist = zeros(num_test)
@@ -46,10 +49,34 @@ def outcome_distribution(env,action:int,num_test:int):
 
 num_episodes = 1000
 env = NormalGame()
-agent = Epsilon_Greedy_Agent(env.num_actions,0.1) #test with epsilon-greedy
-#agent = UCB1_Agent(env.num_actions,100) #or test with UCB1
-temp = train_agent(env,agent,num_episodes)
+#agent = Epsilon_Greedy_Agent(env.num_actions,0.1) #test with epsilon-greedy
+#agent = UCB1_Agent(env.num_actions) #or test with UCB1
+agent = Softmax_Boltzmann_Agent(env.num_actions,10)
+
+#Examples violin plots
+train_agent(env,agent,num_episodes)
 opt_strategy = agent.act()
 distribution = [outcome_distribution(env,action,1000) for action in range(env.num_actions)]
 strategies = list(range(env.num_actions))
 strategies_violin_plots(strategies,distribution,opt_strategy)
+
+#Examples average reward of agents
+num_bandits = 500
+num_episodes = 1000
+rewards_greedy = zeros(num_episodes)
+for _ in range(num_bandits):
+    agent = Epsilon_Greedy_Agent(env.num_actions,0.1)
+    rewards_greedy += train_agent(env,agent,num_episodes)/num_bandits
+rewards_UCB1 = zeros(num_episodes)
+for _ in range(num_bandits):
+    agent = UCB1_Agent(env.num_actions)
+    temp_rewards = train_agent(env,agent,num_episodes)
+    rewards_UCB1 += train_agent(env,agent,num_episodes)/num_bandits
+rewards_boltzmann = zeros(num_episodes)
+for _ in range(num_bandits):
+    agent = Softmax_Boltzmann_Agent(env.num_actions,2)
+    temp_rewards = train_agent(env,agent,num_episodes)
+    rewards_boltzmann += train_agent(env,agent,num_episodes)/num_bandits
+
+reward_plot([rewards_greedy,rewards_UCB1,rewards_boltzmann],["epsilon-greedy","UCB1","Softmax-Boltzmann"])
+
