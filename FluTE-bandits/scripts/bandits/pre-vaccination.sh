494d0c24864247d7904080fcf42a1e1b2c29dfb6
ibos0002 <115453894+ibos0002@users.noreply.github.com>
2022-12-30 15:37:28 +0100

    Added quarantine, saving_dir

--- FluTE-bandits/scripts/bandits/pre-vaccination.sh
+++ FluTE-bandits/scripts/bandits/pre-vaccination.sh
@@ -2,40 +2,27 @@ work_dir=$1
 flute_rl_dir=$2
 flute_rl_bin=$3
 config_template=$4
-seed=$5
-r0=$6
-run_length=$7
-geo=$8
-doses=$9
-priorities=${10}
+saving_dir=$5
+seed=$6
+r0=$7
+run_length=$8
+geo=$9
+doses=${10}
+quarantine=${11}
+priorities=${12}
 priorities_no_commas=`echo ${priorities} | sed 's/,//g'`
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "workdir:${workdir}"
-echo "flute_rl_dir:${flute_rl_dir}"
-echo "flute_rl_bin:${flute_rl_bin}"
-echo "config_template:${config_template}"
-echo "seed:${seed}"
-echo "r0:${r0}"
-echo "run_length:${run_length}"
-echo "geo:${geo}"
-echo "doses:${doses}"
-echo "priorities:${priorities}"
-
 cd ${work_dir}
 
-echo "Creating config file..."
 #create config file
-python $(dirname $PWD)/${flute_rl_dir}/configs/mako-render.py $(dirname $PWD)/rl_dir/${config_template} --label="l0" --seed=${seed} --R0=${r0} --run_length=${run_length} --data_file=$(dirname $PWD)/data/${geo}/${geo} --doses=${doses} --vaccine_priorities=${priorities} > pre.config
-echo "Created config file."
+python3 $(dirname $PWD)/${flute_rl_dir}/configs/mako-render.py $(dirname $PWD)/rl_dir/${config_template} --label="l0" --seed=${seed} --R0=${r0} --run_length=${run_length} --data_file=$(dirname $PWD)/data/${geo}/${geo} --doses=${doses} --vaccine_priorities=${priorities} --quarantine=${quarantine}> pre.config
 
-echo "Running FluTE..."
 #run flute
 $(dirname $PWD)/${flute_rl_bin} pre.config > out 2> err
-echo "Done running FluTE."
 
-echo "Parsing attack rate..."
 #parse attack rate
-ar=`python ${script_dir}/parse_attack_rate.py Summary0`
-echo $ar
+ar=`python3 ${script_dir}/parse_attack_rate.py Summary0`
+mkdir -p $(dirname $PWD)/${saving_dir}/${priorities_no_commas}
+echo $ar >> $(dirname $PWD)/${saving_dir}/${priorities_no_commas}/outcome.txt
