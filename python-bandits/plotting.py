d8f8f005a7ebc9fd2a7653edae228cd305c50b53
ibos0002 <115453894+ibos0002@users.noreply.github.com>
2022-12-30 15:40:35 +0100

    Update plotting.py

--- python-bandits/plotting.py
+++ python-bandits/plotting.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 from typing import Iterable
-from numpy import ndarray, mean
+from numpy import ndarray, mean, argmin
 
-def strategies_violin_plots(strategies:Iterable[int],distributions:Iterable[ndarray],
-                            opt_strategy:int):
+def strategies_violin_plots(strategies:Iterable[int],distributions:Iterable[ndarray]):
     """strategies is a list of integers representing the predictive strategies,
     distributions is a list containing the distribution of the outcome (not the reward) of each
     of the strategies in strategies. opt_strategy is the integer representing the optimal strategy"""
     means = [mean(distributions[strat]) for strat in strategies]
-    plt.violinplot(distributions,strategies)
+    opt_strategy = argmin(means)
+    plt.figure(figsize=(10,6))
+    parts = plt.violinplot(distributions,strategies,widths=0.8,showextrema=False)
+    for pc in parts['bodies']:
+        pc.set_facecolor('teal')
+        pc.set_edgecolor('black')
+        pc.set_alpha(0.5)
     plt.scatter(strategies,means,c="r",marker="D")
     plt.xlabel("Strategy",fontsize=12)
     plt.ylabel("Outcome",fontsize=12)
     plt.xticks(strategies,fontsize=12)
     plt.yticks(fontsize=12)
+    plt.xlim(-1,32)
     plt.axhline(y=mean(distributions[opt_strategy]),c="r",linestyle="--")
     plt.show()
 
