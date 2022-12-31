e7d665dd726fde726383e5fd6588e96c398326bf
ibos0002 <115453894+ibos0002@users.noreply.github.com>
2022-12-31 10:14:42 +0100

    Add function to plot optimal action

--- python-bandits/plotting.py
+++ python-bandits/plotting.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 from typing import Iterable
-from numpy import ndarray, mean, argmin
+from numpy import ndarray, mean, argmin, zeros
+
+strategies = ["1,0,0,0,0","0,1,0,0,0","0,0,1,0,0","0,0,0,1,0","0,0,0,0,1","1,1,0,0,0","1,0,1,0,0",
+              "1,0,0,1,0","1,0,0,0,1","0,1,1,0,0","0,1,0,1,0","0,1,0,0,1","0,0,1,1,0","0,0,1,0,1",
+              "0,0,0,1,1","0,0,1,1,1","0,1,0,1,1","0,1,1,0,1","0,1,1,1,0","1,0,0,1,1","1,0,1,0,1",
+              "1,0,1,1,0","1,1,0,0,1","1,1,0,1,0","1,1,1,0,0","0,1,1,1,1","1,0,1,1,1","1,1,0,1,1",
+              "1,1,1,0,1","1,1,1,1,0","1,1,1,1,1","0,0,0,0,0"]
 
 def strategies_violin_plots(strategies:Iterable[int],distributions:Iterable[ndarray]):
     """strategies is a list of integers representing the predictive strategies,
@@ -30,6 +36,24 @@ def reward_plot(rewards:Iterable[ndarray],agent_names:Iterable[str]):
         plt.plot(rewards[i])
     plt.xlabel("Iterations",fontsize=12)
     plt.ylabel("Average reward",fontsize=12)
+    plt.legend(agent_names)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.show()
+
+def optimal_strat_plot(actions:Iterable[Iterable[ndarray]],agent_names:Iterable[str],optimal_strategy:int):
+    """actions = list containing for each agent type a the actions done by the agent type
+    over a pre-defined number agents, agent_names = names of the agent types"""
+    optimal_perc = [zeros(actions[0][0].shape) for _ in agent_names]
+    for ag in range(len(agent_names)):
+        for action_list in actions[ag]:
+            for t in range(len(action_list)):
+                if action_list[t] == optimal_strategy:
+                    optimal_perc[ag][t] += 1/len(actions[ag])
+    for ag in range(len(agent_names)):
+        plt.plot(optimal_perc[ag])
+    plt.xlabel("Iterations",fontsize=12)
+    plt.ylabel("Optimal actions (%)",fontsize=12)
     plt.legend(agent_names)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
