3531c17b25c1b597068871c884cbe30e6c3603bc
ibos0002 <115453894+ibos0002@users.noreply.github.com>
2023-01-08 10:13:25 +0100

    Change font sizes

--- python-bandits/plotting.py
+++ python-bandits/plotting.py
@@ -16,26 +16,27 @@ def strategies_violin_plots(strategies:Iterable[int],distributions:Iterable[ndar
         pc.set_edgecolor('black')
         pc.set_alpha(0.5)
     plt.scatter(strategies,means,c="r",marker="D")
-    plt.xlabel("Strategy",fontsize=12)
-    plt.ylabel("Outcome",fontsize=12)
+    plt.xlabel("Strategy",fontsize=14)
+    plt.ylabel("Outcome",fontsize=14)
     plt.xticks(strategies,fontsize=12)
     plt.yticks(fontsize=12)
     plt.xlim(-1,len(strategies))
     plt.axhline(y=mean(distributions[opt_strategy]),c="r",linestyle="--")
-    plt.title(title)
+    plt.title(title,fontsize=16)
     plt.show()
 
 def reward_plot(rewards:Iterable[ndarray],agent_names:Iterable[str],title:str):
     """rewards = list of rewards over 1000 times steps averaged using 500 bandits for each agent type
     agent_names = names of the algorithms used for each agent"""
+    plt.figure(figsize=(10,6))
     for i in range(len(rewards)):
         plt.plot(rewards[i])
-    plt.xlabel("Iterations",fontsize=12)
-    plt.ylabel("Average reward",fontsize=12)
+    plt.xlabel("Iterations",fontsize=14)
+    plt.ylabel("Average reward",fontsize=14)
     plt.legend(agent_names)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
-    plt.title(title)
+    plt.title(title,fontsize=16)
     plt.show()
 
 def optimal_strat_plot(actions:Iterable[Iterable[ndarray]],agent_names:Iterable[str],optimal_strategy:int,title:str):
@@ -47,25 +48,26 @@ def optimal_strat_plot(actions:Iterable[Iterable[ndarray]],agent_names:Iterable[
             for t in range(len(action_list)):
                 if action_list[t] == optimal_strategy:
                     optimal_perc[ag][t] += 1/len(actions[ag])
+    plt.figure(figsize=(10,6))
     for ag in range(len(agent_names)):
         plt.plot(optimal_perc[ag])
-    plt.xlabel("Iterations",fontsize=12)
-    plt.ylabel("Optimal actions (%)",fontsize=12)
+    plt.xlabel("Iterations",fontsize=14)
+    plt.ylabel("Optimal actions (%)",fontsize=14)
     plt.legend(agent_names)
     plt.xticks(fontsize=12)
     plt.yticks(fontsize=12)
-    plt.title(title)
+    plt.title(title,fontsize=16)
     plt.show()
 
 def variance_plot(strategies:Iterable[int],distributions:Iterable[ndarray],title:str):
     variance_List = []
     for strat in strategies:
         variance_List.append(var(distributions[strat]))
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(10,6))
     plt.bar(strategies,variance_List)
-    plt.title(title)
-    plt.xlabel("Iterations",fontsize=12)
-    plt.ylabel("Variance",fontsize=12)
+    plt.title(title,fontsize=16)
+    plt.xlabel("Iterations",fontsize=14)
+    plt.ylabel("Variance",fontsize=14)
     plt.xticks(strategies,fontsize=12)
     plt.yticks(fontsize=12)
     plt.xlim(-1,len(strategies))
